ff5b8570760adc5df15c16cfe955fbe874e03b47
ayeshaawais2907-maker <ayeshaawais2907@gmail.com>
2026-01-17 21:04:52 +0500

    I made a project

---
+++ script.js
@@ -0,0 +1,440 @@
+const items = [
+  {
+    id: 1,
+    name: "Lavender Bead Bracelet",
+    price: 999,
+    category: "bracelets",
+    img: "images2/Lavenadr bracelet.png",
+    newArrival: true,
+    bestSeller: true
+  },
+  {
+    id: 2,
+    name: "Pearl Necklace",
+    price: 1499,
+    category: "necklaces",
+    img: "images/pearl necklace.png",
+    newArrival: false,
+    bestSeller: true
+  },
+  {
+    id: 3,
+    name: "Butterfly Keychain",
+    price: 699,
+    category: "keychains",
+    img: "images2/butterfly keychain.png",
+    newArrival: true,
+    bestSeller: true
+  },
+  {
+    id: 4,
+    name: "Gem Earrings",
+    price: 1199,
+    category: "earrings",
+    img: "images2/gem earrings.png",
+    newArrival: false,
+    bestSeller: true
+  },
+  {
+    id: 5,
+    name: "Grape Earrings",
+    price: 650,
+    category: "earrings",
+    img: "images2/grape earrings.png",
+    newArrival: true,
+    bestSeller: true
+  },
+  {
+    id: 6,
+    name: "Rainbow Earrings",
+    price: 550,
+    category: "earrings",
+    img: "images2/gradient earrings.png",
+    newArrival: false,
+    bestSeller: false
+  },
+  {
+    id: 7,
+    name: "Black Flower Phonecharms",
+    price: 550,
+    category: "phonecharms",
+    img: "images2/black flower charm.png",
+    newArrival: false,
+    bestSeller: false
+  },
+  {
+    id: 8,
+    name: "Pink Bow Phonecharm",
+    price: 1150,
+    category: "phonecharms",
+    img: "images/pink bow charm.png",
+    newArrival: false,
+    bestSeller: false
+  },
+  {
+    id: 9,
+    name: "Sheer Flower Phonecharm",
+    price: 675,
+    category: "phonecharms",
+    img: "images/sheer flower charm.png",
+    newArrival: true,
+    bestSeller: false
+  },
+  {
+    id: 10,
+    name: "Seashell Phonecharm",
+    price: 550,
+    category: "phonecharms",
+    img: "images/seashell charm.png",
+    newArrival: false,
+    bestSeller: false
+  },
+  {
+    id: 11,
+    name: "Sunflower Phonecharm",
+    price: 850,
+    category: "phonecharms",
+    img: "images/sunflower charm.png",
+    newArrival: false,
+    bestSeller: false
+  },
+  {
+    id: 12,
+    name: "Bow Keychain",
+    price: 650,
+    category: "keychains",
+    img: "images2/bow keychain.png",
+    newArrival: false,
+    bestSeller: false
+  },
+  {
+    id: 13,
+    name: "Fruit Keychain",
+    price: 425,
+    category: "keychains",
+    img: "images2/fruit keychain.png",
+    newArrival: false,
+    bestSeller: false
+  },
+  {
+    id: 14,
+    name: "Jasmine Keychain",
+    price: 425,
+    category: "keychains",
+    img: "images2/jasmine keychain.png",
+    newArrival: true,
+    bestSeller: true
+  },
+  {
+    id: 15,
+    name: "Tropical Keychain",
+    price: 350,
+    category: "keychains",
+    img: "images/tropical keychain.png",
+    newArrival: false,
+    bestSeller: false
+  },
+  {
+    id: 16,
+    name: "Ghost Necklace",
+    price: 850,
+    category: "necklaces",
+    img: "images2/ghost necklace.png",
+    newArrival: false,
+    bestSeller: false
+  },
+  {
+    id: 17,
+    name: "Friendship Heart Necklace",
+    price: 1450,
+    category: "necklaces",
+    img: "images2/friendship heart necklace.png",
+    newArrival: false,
+    bestSeller: true
+  },
+  {
+    id: 18,
+    name: "Seashell Necklace",
+    price: 850,
+    category: "necklaces",
+    img: "images/seashell necklace.png",
+    newArrival: false,
+    bestSeller: false
+  },
+  {
+    id: 19,
+    name: "Yellow Bead Bracelet",
+    price: 780,
+    category: "bracelets",
+    img: "images/yellow beads bracelet.png",
+    newArrival: false,
+    bestSeller: false
+  },
+  {
+    id: 20,
+    name: "Ghost Charm Bracelet",
+    price: 299,
+    category: "bracelets",
+    img: "images2/ghost bracelet.png",
+    newArrival: false,
+    bestSeller: false
+  },
+  {
+    id: 21,
+    name: "Sunflower Bracelet",
+    price: 990,
+    category: "bracelets",
+    img: "images/sunflower bracelet.png",
+    newArrival: false,
+    bestSeller: true
+  },
+  {
+    id: 22,
+    name: "Ocean Gradient Earrings",
+    price: 1700,
+    category: "earrings",
+    img: "images/ocean earrings.png",
+    newArrival: true,
+    bestSeller: true
+  },
+  {
+    id: 23,
+    name: "Purple Bead Earrings",
+    price: 750,
+    category: "earrings",
+    img: "images/purple bead earrings.png",
+    newArrival: false,
+    bestSeller: false
+  },
+  {
+    id: 24,
+    name: "Rainbow Earrings",
+    price: 990,
+    category: "earrings",
+    img: "images/rainbow earrings.png",
+    newArrival: false,
+    bestSeller: false
+  },
+  {
+    id: 25,
+    name: "Read Spiral Earrings",
+    price: 990,
+    category: "earrings",
+    img: "images/red bead earrings.png",
+    newArrival: false,
+    bestSeller: false
+  },
+   {
+    id: 26,
+    name: "Pink Flower Phonecharm",
+    price: 490,
+    category: "phonecharms",
+    img: "images/pink flower charms.png",
+    newArrival: false,
+    bestSeller: false
+  },
+   {
+    id: 27,
+    name: "Red Heart Phonecharm",
+    price: 410,
+    category: "phonecharms",
+    img: "images/red heart charms.png",
+    newArrival: false,
+    bestSeller: false
+  },
+  {
+    id: 28,
+    name: "Multi-bead Keychain",
+    price: 550,
+    category: "keychains",
+    img: "images2/multibead keychain.png",
+    newArrival: false,
+    bestSeller: false
+  },
+  {
+    id: 29,
+    name: "Colorful Tassel Keychain",
+    price: 650,
+    category: "keychains",
+    img: "images/tassel keychain.png",
+    newArrival: false,
+    bestSeller: false
+  },
+  {
+    id: 30,
+    name: "Aquamarine Bead Bracelet",
+    price: 780,
+    category: "bracelets",
+    img: "images2/aquamarine bead bracelet.png",
+    newArrival: false,
+    bestSeller: false
+  },
+  {
+    id: 31,
+    name: "Pink Jasmine Bracelet",
+    price: 780,
+    category: "bracelets",
+    img: "images/pink jasmine bracelet.png",
+    newArrival: false,
+    bestSeller: false
+  },
+  {
+    id: 32,
+    name: "Unicorn Charm Bracelet",
+    price: 595,
+    category: "bracelets",
+    img: "images/unicorn bracelet.png",
+    newArrival: true,
+    bestSeller: false
+  },
+  {
+    id: 33,
+    name: "Blue Beaded Necklace",
+    price: 1450,
+    category: "necklaces",
+    img: "images2/blue beaded necklace.png",
+    newArrival: false,
+    bestSeller: false
+  },
+  {
+    id: 34,
+    name: "Crushed Blue Necklace",
+    price: 1100,
+    category: "necklaces",
+    img: "images2/Crushed blue chain.png",
+    newArrival: true,
+    bestSeller: false
+  },
+  {
+    id: 35,
+    name: "Multi-beaded Necklace",
+    price: 1450,
+    category: "necklaces",
+    img: "images2/Multi-beaded necklace.png",
+    newArrival: false,
+    bestSeller: false
+  }
+];
+
+const shop = document.getElementById("shop-items");
+const best = document.getElementById("best-sellers");
+const page = window.location.pathname.split("/").pop().split(".")[0];
+
+items.forEach(item => {
+  if (best && page === "home" && item.bestSeller) {
+    addCard(best, item);
+  }
+
+  if (shop && page === "shop") {
+    addCard(shop, item);
+  }
+
+  if (shop && page === item.category) {
+    addCard(shop, item);
+  }
+
+  if (shop && page === "new" && item.newArrival) {
+    addCard(shop, item);
+  }
+});
+
+let cart = [];
+
+function addCard(container, item) {
+  const card = document.createElement("div");
+  card.className = "card";
+  card.onclick = () => openProductModal(item.id); 
+  
+  card.innerHTML = `
+    <img src="${item.img}" class="product-img">
+    <h3>${item.name}</h3>
+    <button onclick="event.stopPropagation(); addToCart(${item.id})">Add to Cart</button>
+  `;
+  container.appendChild(card);
+}
+function openProductModal(id) {
+  const item = items.find(i => i.id === id);
+  if (!item) return;
+
+  const detailDiv = document.getElementById("productDetailContent");
+  
+  detailDiv.innerHTML = `
+    <div class="product-form">
+      <img src="${item.img}" style="width:100%; max-height:200px; object-fit:contain;">
+      <p><strong>Name:</strong> ${item.name}</p>
+      <p><strong>Price:</strong> Rs ${item.price}</p>
+      <p><strong>Description:</strong> ${item.description || "No description available."}</p>
+      <hr>
+      <button id="Btncart" onclick="addToCart(${item.id}); closeProductModal()">Add to Cart Now</button>
+    </div>
+  `;
+
+  document.getElementById("productModal").style.display = "flex";
+}
+
+function closeProductModal() {
+  document.getElementById("productModal").style.display = "none";
+}
+
+function addToCart(id) {
+  const item = items.find(i => i.id === id);
+  if (item) {
+    cart.push(item);
+    alert(`${item.name} added to cart!`);
+  }
+}
+
+function openCart() {
+  document.getElementById("cartModal").style.display = "flex";
+  displayCartItems();
+}
+
+function closeCart() {
+  document.getElementById("cartModal").style.display = "none";
+}
+
+function displayCartItems() {
+  const cartDiv = document.getElementById("cartItems");
+  cartDiv.innerHTML = "";
+  if(cart.length === 0){
+    cartDiv.innerHTML = "<p>Cart is empty</p>";
+    return;
+  }
+  cart.forEach((item, index) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+      ${item.name} - Rs ${item.price} 
+      <button onclick="removeFromCart(${index})">Remove</button>
+    `;
+    cartDiv.appendChild(div);
+  });
+}
+
+function removeFromCart(index) {
+  cart.splice(index, 1);
+  displayCartItems();
+}
+
+function checkout() {
+  const name = document.getElementById("custName").value.trim();
+  const phone = document.getElementById("custPhone").value.trim();
+  const address = document.getElementById("custAddress").value.trim();
+  const email = document.getElementById("custEmail").value.trim();
+
+  if (!name || !phone || !address || !email) {
+    alert("You have to fill all fields!");
+    return;
+  }
+
+  if(cart.length === 0){
+    alert("Your cart is empty!");
+    return;
+  }
+
+  alert(`Thank you, ${name}! Your order has been placed.`);
+  cart = [];
+  closeCart();
+}
+
+
+
